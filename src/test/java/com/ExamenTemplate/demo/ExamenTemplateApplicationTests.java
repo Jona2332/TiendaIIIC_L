b1ace472b6832fa2eea29833e84c63415f2eb100
Jonathan Anchia <jonhanchia2@gmail.com>
2022-10-24 15:54:20 -0600

    Test_Primero

---
+++ src/test/java/com/ExamenTemplate/demo/ExamenTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExamenTemplate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
