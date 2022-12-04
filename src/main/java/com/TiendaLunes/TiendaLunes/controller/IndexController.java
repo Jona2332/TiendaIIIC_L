e539d89000fc7cb996d5cd6f85f19aee9d1ff4af
Jonathan Anchia <jonhanchia2@gmail.com>
2022-12-04 10:01:17 -0600

    Semana 5

--- src/main/java/com/TiendaLunes/TiendaLunes/controller/IndexController.java
+++ src/main/java/com/TiendaLunes/TiendaLunes/controller/IndexController.java
@@ -1,26 +1,38 @@
 
 package com.TiendaLunes.TiendaLunes.controller;
 
-import com.TiendaLunes.TiendaLunes.dao.ClienteDAO;
+
 import com.TiendaLunes.TiendaLunes.domain.Cliente;
-import java.util.Arrays;
+import com.TiendaLunes.TiendaLunes.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class IndexController {
     @Autowired
-    private ClienteDAO clienteDao;
+    private ClienteService clienteService;
     @GetMapping("/")
     public String inicio(Model model){
         log.info("Estamos usando una arquitectura MVC");
         
-        var clientes=clienteDao.findAll();
+        var clientes=clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+        
+    }
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+    
+        clienteService.save(cliente);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/TiendaLunes/TiendaLunes/service/ClienteServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.TiendaLunes.TiendaLunes.service;
+
+import com.TiendaLunes.TiendaLunes.dao.ClienteDAO;
+import com.TiendaLunes.TiendaLunes.domain.Cliente;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author jonha
+ */
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+    @Autowired
+    private ClienteDAO clienteDao;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> getClientes() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Cliente cliente) {
+        clienteDao.delete(cliente);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente getCliente(Cliente cliente) {
+       return clienteDao.findById(cliente.getIdCliente()).orElse(null);
+    }
+    
+}
